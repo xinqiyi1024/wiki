f4555a5bca36b239841464edf7dbab8118969971
xinqiyi1024 <3633414863@qq.com>
2022-04-05 09:42:55 +0800

    使用test.http测试hello接口

--- src/main/java/com/fzn/wiki/controller/TestController.java
+++ src/main/java/com/fzn/wiki/controller/TestController.java
@@ -1,7 +1,7 @@
 package com.fzn.wiki.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,5 +14,10 @@ public class TestController {
     @GetMapping("/hello")
     public String hello() {
         return "hello";
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name) {
+        return "hello post " + name;
     }
 }
