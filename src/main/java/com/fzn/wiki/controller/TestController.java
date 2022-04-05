1ee4710a0570b8eb794a65c7484833b090fc45b7
xinqiyi1024 <3633414863@qq.com>
2022-04-05 10:09:15 +0800

    增加自定义配项的实例

--- src/main/java/com/fzn/wiki/controller/TestController.java
+++ src/main/java/com/fzn/wiki/controller/TestController.java
@@ -1,5 +1,6 @@
 package com.fzn.wiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class TestController {
+    @Value("${test.hello:def}")
+    private String testHello;
+
     @GetMapping("/hello")
     public String hello() {
-        return "hello";
+        return "hello==>" + testHello;
     }
 
     @PostMapping("/hello/post")
