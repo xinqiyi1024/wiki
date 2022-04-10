fd143b983e3d2088c34e3d44ebc84fa7b2df272b
xinqiyi1024 <3633414863@qq.com>
2022-04-10 14:43:58 +0800

    add vue多环境配置，增加dev和prod配置文件 编译和启动命令增加多环境

--- web/src/main.ts
+++ web/src/main.ts
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import 'ant-design-vue/dist/antd.css'
 import * as Icons from '@ant-design/icons-vue'
 
 const app = createApp(App);
@@ -14,3 +14,6 @@ const icons: any = Icons;
 for (const i in icons) {
     app.component(i, icons[i])
 }
+
+console.log('环境', process.env.NODE_ENV)
+console.log('服务端', process.env.VUE_APP_SERVER)
