16ded1dee8931ceae957220e585849c5691b4203
xinqiyi1024 <3633414863@qq.com>
2022-04-11 16:07:09 +0800

    feature 集成pageHelper插件将mapper日志级别改为trace，打印所有sql日志

--- src/main/java/com/fzn/wiki/service/BookService.java
+++ src/main/java/com/fzn/wiki/service/BookService.java
@@ -6,6 +6,10 @@ import com.fzn.wiki.domain.request.BookRequest;
 import com.fzn.wiki.domain.response.BookResponse;
 import com.fzn.wiki.mapper.BookMapper;
 import com.fzn.wiki.util.CopyUtil;
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
@@ -19,19 +23,23 @@ import java.util.List;
  */
 @Service
 public class BookService {
+    private static final Logger LOG = LoggerFactory.getLogger(BookService.class);
     @Resource
     private BookMapper bookMapper;
-
-    public List<Book> list() {
-        return bookMapper.selectByExample(null);
-    }
 
     public List<BookResponse> listByName(BookRequest req) {
         BookExample bookExample = new BookExample();
         if (!ObjectUtils.isEmpty(req.getName())) {
             bookExample.createCriteria().andNameLike(req.getName() + "%");
         }
+        // 前段传过来
+        PageHelper.startPage(1, 3);
         List<Book> bookList = bookMapper.selectByExample(bookExample);
+
+        PageInfo<Book> pageInfo = new PageInfo<>(bookList);
+        // 返回list和总条数
+        LOG.info("总条数：{}", pageInfo.getTotal());
+        LOG.info("总页数：{}", pageInfo.getPages());
 
         List<BookResponse> responseList = CopyUtil.copyList(bookList, BookResponse.class);
         return responseList;
