ccf5c9d79511eb1782c56aee95f3316bad9f6d3a
xinqiyi1024 <3633414863@qq.com>
2022-04-12 10:10:05 +0800

    add 前后端电子书分页整合

--- web/src/views/admin/AdminBook.vue
+++ web/src/views/admin/AdminBook.vue
@@ -39,7 +39,7 @@ export default defineComponent({
     const books = ref();
     const pagination = ref({
       current: 1,
-      pageSize: 9,
+      pageSize: 4,
       total: 0
     });
     const loading = ref(false);
@@ -86,17 +86,23 @@ export default defineComponent({
      * 数据查询
      **/
     const handleQuery = (params: any) => {
-      loading.value = true;
+      loading.value = true
       // 如果不清空现有数据，则编辑保存重新加载数据后，再点编辑，则列表显示的还是编辑前的数据
-      axios.get("/book/list", params).then((response) => {
-        loading.value = false;
-        const data = response.data;
-        books.value = data.content;
+      axios.get("/book/list", {
+        params: {
+          page: params.page,
+          size: params.size
+        }
+      }).then((response) => {
+        loading.value = false
+        const data = response.data
+        books.value = data.content.list
 
         // 重置分页按钮
-        pagination.value.current = params.page;
-      });
-    };
+        pagination.value.current = params.page
+        pagination.value.total = data.content.total
+      })
+    }
 
     /**
      * 表格点击页码时触发
@@ -110,7 +116,10 @@ export default defineComponent({
     }
 
     onMounted(() => {
-      handleQuery({});
+      handleQuery({
+        page: 1,
+        size: pagination.value.pageSize
+      });
     });
 
     return {
